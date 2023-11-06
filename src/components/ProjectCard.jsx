caa9e0bf41b46262737e1b41ab1b0a16cb421ae3
syedfaheem27 <syedfaheem2719@gmail.com>
2023-11-07 01:33:14 +0530

    Remove ProjectsCard component

--- src/components/ProjectCard.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProjectCard = () => {
-  return <div></div>;
-};
-
-export default ProjectCard;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import styles from "./Projects.module.css";
 const Projects = () => {
   return (
-    <div className={styles.container}>
-      <header>
-        <h1>My Projects</h1>
-      </header>
-      <section className={styles.projects}></section>
+    <div>
+      <h1>Projects</h1>
     </div>
   );
 };
