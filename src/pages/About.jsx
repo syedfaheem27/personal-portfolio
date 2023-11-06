b16b86aa03edacf99eb637aedcaa7a7df272c5f2
syedfaheem27 <syedfaheem2719@gmail.com>
2023-11-07 00:55:25 +0530

    Finish about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,93 +13,122 @@ const About = () => {
     setShowToast(true);
   }
   return (
-    <section id="about" className={styles.container}>
-      {showToast && <div className={styles.toast}>✅ Email copied.</div>}
+    <>
+      <div className={styles.container}>
+        <header>
+          <h1>Hi, I'm Syed Faheem </h1>
+        </header>
+        <section id="about">
+          {showToast && <div className={styles.toast}>✅ Email copied.</div>}
 
-      <header>
-        <h1>Hi, I'm Syed Faheem </h1>
-      </header>
-      <section className={styles.content}>
-        <p>
-          I am a self-taught front-end developer based in India. As someone, who
-          has always loved mathematics, I enjoy transforming complex problems
-          into simple and intuitive solutions. I love to explore and learn new
-          things and i go with the philosophy that i never know anything which
-          always keeps me motivated to search for more and never settle for
-          less. Currently, most of my endeavours are in javascript and React.js
-        </p>
-        <p>
-          Aside from that, i love <span>reading</span>, <span>football</span>{" "}
-          and <span>hitting the gym</span>. When i am not coding, you'll
-          probably find me playing football or at the gym as i truly believe
-          that keeping the body in good health is our duty, otherwise we shall
-          not be able to keep the mind stronger and clear.
-        </p>
-        <p>
-          As someone with a keen interest in tech and a knack for
-          problem-solving, web development was an obvious path.
-        </p>
-        <p>
-          Let's get in touch. Write me at{"  "}
-          <button onClick={copyMail}>faheem.2719.dev@gmail.com</button>
-        </p>
-      </section>
-      <section id="social-media-handles" className={styles.social}>
-        <a href="mailto:syedfaheem132@gmail.com">
-          <img src="../assets/mail-outline.svg" alt="Mail" />
-        </a>
-        <a href="https://github.com/syedfaheem27" target="blank">
-          <img src="../assets/logo-github.svg" alt="github" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/syed-faheem-6285a0261/"
-          target="blank"
-        >
-          <img src="../assets/logo-linkedin.svg" alt="linked-in" />
-        </a>
-        <a href="https://twitter.com/faheemsyed_1" target="blank">
-          <img src="../assets/logo-twitter.svg" alt="twitter" />
-        </a>
-      </section>
+          <section className={styles.content}>
+            <p>
+              I am a self-taught front-end developer based in India. As someone,
+              who has always loved mathematics, I enjoy transforming complex
+              problems into simple and intuitive solutions. I love to explore
+              and learn new things and i go with the philosophy that i never
+              know anything which always keeps me motivated to search for more
+              and never settle for less. Currently, most of my endeavours are in
+              javascript and React.js
+            </p>
+            <p>
+              Aside from that, i love <span>reading</span>,{" "}
+              <span>football</span> and <span>hitting the gym</span>. When i am
+              not coding, you'll probably find me playing football or at the gym
+              as i truly believe that keeping the body in good health is our
+              duty, otherwise we shall not be able to keep the mind stronger and
+              clear.
+            </p>
+            <p>
+              As someone with a keen interest in tech and a knack for
+              problem-solving, web development was an obvious path.
+            </p>
+            <p>
+              Let's get in touch. Write me at{" "}
+              <button onClick={copyMail}> faheem.2719.dev@gmail.com</button>
+            </p>
+          </section>
+          <section id="social-media-handles" className={styles.social}>
+            <a href="mailto:syedfaheem132@gmail.com">
+              <img src="../assets/mail-outline.svg" alt="Mail" />
+            </a>
+            <a href="https://github.com/syedfaheem27" target="blank">
+              <img src="../assets/logo-github.svg" alt="github" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/syed-faheem-6285a0261/"
+              target="blank"
+            >
+              <img src="../assets/logo-linkedin.svg" alt="linked-in" />
+            </a>
+            <a href="https://twitter.com/faheemsyed_1" target="blank">
+              <img src="../assets/logo-twitter.svg" alt="twitter" />
+            </a>
+          </section>
 
-      <section id="skills" className={styles.skills}>
-        <h2>My Skills</h2>
-        <div className={styles.underline}></div>
-        <div>
-          <h3>Languages</h3>
-          <div className={styles["icon-container"]}>
-            <img src="../solidIcons/HTML_5.svg" alt="HTML5 logo" />
-            <img src="../solidIcons/CSS_3.svg" alt="CSS3 logo" />
-            <img src="../solidIcons/JS.svg" alt="Java-Script logo" />
-            <img src="../solidIcons/C.svg" alt="C Programming logo" />
-          </div>
-        </div>
-        <div>
-          <h3>Technologies</h3>
-          <div className={styles["icon-container"]}>
-            <img src="../solidIcons/react.svg" alt="React logo" />
+          <section id="skills" className={styles.skills}>
+            <h2>My Skills</h2>
+            <div className={styles.underline}></div>
+            <div>
+              <h3>Languages</h3>
+              <div className={styles["icon-container"]}>
+                <img src="../solidIcons/HTML_5.svg" alt="HTML5 logo" />
+                <img src="../solidIcons/CSS_3.svg" alt="CSS3 logo" />
+                <img src="../solidIcons/JS.svg" alt="Java-Script logo" />
+                <img src="../solidIcons/C.svg" alt="C Programming logo" />
+              </div>
+            </div>
+            <div>
+              <h3>Technologies</h3>
+              <div className={styles["icon-container"]}>
+                <img src="../solidIcons/react.svg" alt="React logo" />
 
-            <img src="../solidIcons/redux.svg" alt="Redux logo" />
+                <img src="../solidIcons/redux.svg" alt="Redux logo" />
 
-            <img src="../solidIcons/bootstrap.svg" alt="Bootstrap logo" />
+                <img src="../solidIcons/bootstrap.svg" alt="Bootstrap logo" />
 
-            <img src="../solidIcons/tailwind.svg" alt="Tailwind logo" />
-          </div>
-        </div>
-        <div>
-          <h3>Other Tools</h3>
-          <div className={styles["icon-container"]}>
-            <img src="../solidIcons/linux.svg" alt="Linux logo" />
+                <img src="../solidIcons/tailwind.svg" alt="Tailwind logo" />
+              </div>
+            </div>
+            <div>
+              <h3>Other Tools</h3>
+              <div className={styles["icon-container"]}>
+                <img src="../solidIcons/linux.svg" alt="Linux logo" />
 
-            <img src="../solidIcons/git.svg" alt="Git logo" />
+                <img src="../solidIcons/git.svg" alt="Git logo" />
 
-            <img src="../solidIcons/VS-Code.svg" alt="Vs Code logo" />
+                <img src="../solidIcons/VS-Code.svg" alt="Vs Code logo" />
 
-            <img src="../solidIcons/NPM.svg" alt="Npm logo" />
-          </div>
-        </div>
-      </section>
-    </section>
+                <img src="../solidIcons/NPM.svg" alt="Npm logo" />
+              </div>
+            </div>
+          </section>
+        </section>
+      </div>
+      <footer className={styles.footer}>
+        <section className={styles.copy}>
+          <span>&copy;</span>
+          <strong>syed faheem</strong>
+        </section>
+        <section className={styles.social}>
+          <a href="mailto:syedfaheem132@gmail.com">
+            <img src="../assets/mail-outline.svg" alt="Mail" />
+          </a>
+          <a href="https://github.com/syedfaheem27" target="blank">
+            <img src="../assets/logo-github.svg" alt="github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/syed-faheem-6285a0261/"
+            target="blank"
+          >
+            <img src="../assets/logo-linkedin.svg" alt="linked-in" />
+          </a>
+          <a href="https://twitter.com/faheemsyed_1" target="blank">
+            <img src="../assets/logo-twitter.svg" alt="twitter" />
+          </a>
+        </section>
+      </footer>
+    </>
   );
 };
 
